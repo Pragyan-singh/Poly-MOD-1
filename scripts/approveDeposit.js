ac4c47dbd55a5cdb471d6e93de840fb383add1d9
Pragyan-singh <126009063+Pragyan-singh@users.noreply.github.com>
2023-07-30 00:02:53 +0530

    Update approveDeposit.js

--- scripts/approveDeposit.js
+++ scripts/approveDeposit.js
@@ -7,8 +7,8 @@
 const hre = require("hardhat");
 const fxRootContractABI = require("../fxRootContractABI.json");
 const tokenContractJSON = require("../artifacts/contracts/MetaToken.sol/MetaToken.json");
-var i=0;
-const tokenAddress = "0xdB4a0DD31216EE77e8ff63eeBF924cb7611841CB"; // place your erc20 contract address here
+
+const tokenAddress = "0xCae322ddD4BE401D7d6C390Bde14DEE558E28793"; // place your erc20 contract address here
 const tokenABI = tokenContractJSON.abi;
 const fxERC20RootAddress = "0xF9bc4a80464E48369303196645e876c8C7D972de";
 const walletAddress = "0xBeeF8E9b0eABa30e7c1DF83675780C784aa78e2c"; // place your public address for your wallet here
@@ -18,7 +18,7 @@ async function main() {
     const tokenContract = await hre.ethers.getContractAt(tokenABI, tokenAddress);
     const fxContract = await hre.ethers.getContractAt(fxRootContractABI, fxERC20RootAddress);
 
-    for(i=1;i<=5;i++)
+    for(var i=1;i<=5;i++)
     {
       const approveTx = await tokenContract.approve(fxERC20RootAddress, i);
       await approveTx.wait();
@@ -28,11 +28,12 @@ async function main() {
     console.log('Approval confirmed');
 
 
-    for(i=1;i<=5;i++)
+    for(var i=1;i<=5;i++)
     {
       const depositTx = await fxContract.deposit(tokenAddress, walletAddress, i, "0x6556");
       await depositTx.wait();
     }
+    
 
     console.log("Tokens deposited");
   
