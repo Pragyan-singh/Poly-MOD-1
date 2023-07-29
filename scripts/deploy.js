c49aae94646fd61fd9ca9270897e4fbc71d0e09a
Pragyan-singh <126009063+Pragyan-singh@users.noreply.github.com>
2023-07-30 00:03:21 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -8,7 +8,7 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const token = await hre.ethers.deployContract("Poly");
+  const token = await hre.ethers.deployContract("MetaToken");
 
   console.log("Token address:", await token.getAddress());
 }
